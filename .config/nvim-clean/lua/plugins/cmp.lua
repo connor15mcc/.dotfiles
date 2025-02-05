cc94aa5db7512dae444f24e028f2736448195555
Connor McCarthy <connor15mcc@gmail.com>
2025-02-05 13:04:30 -0500

    Get auto-LSP, move back to nvim-cmp
    I like the modal cmp style (blink doesn't support)

--- .config/nvim-clean/lua/plugins/cmp.lua
+++ .config/nvim-clean/lua/plugins/cmp.lua
@@ -1,29 +1,56 @@
 return {
-	"saghen/blink.cmp",
-	lazy = false,
-	version = "v0.*",
-	opts = {
-		keymap = {
-			["<C-p>"] = { "select_prev" },
-			["<C-n>"] = { "select_next" },
-			["<C-b>"] = { "scroll_documentation_down" },
-			["<C-f>"] = { "scroll_documentation_up" },
-			["<C-j>"] = { "cancel" },
-			["<C-k>"] = { "select_and_accept" },
-		},
-		completion = {
-			menu = {
-				draw = {
-					columns = {
-						{ "label", "label_description" },
-					},
-				},
-			},
-		},
-
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
-	},
-	opts_extend = { "sources.default" },
+  "hrsh7th/nvim-cmp",
+  version = false, -- last release is way too old
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-emoji",
+    "saadparwaiz1/cmp_luasnip",
+  },
+  opts = function()
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+    local cmp = require("cmp")
+    local defaults = require("cmp.config.default")()
+    return {
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-j>"] = cmp.mapping.abort(),
+        ["<C-k>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "emoji" },
+        { name = "luasnip" },
+        { name = "path" },
+      }, {
+        { name = "buffer" },
+      }),
+      experimental = {
+        ghost_text = {
+          hl_group = "CmpGhostText",
+        },
+      },
+      sorting = defaults.sorting,
+    }
+  end,
+  ---@param opts cmp.ConfigSchema
+  config = function(_, opts)
+    for _, source in ipairs(opts.sources) do
+      source.group_index = source.group_index or 1
+    end
+    require("cmp").setup(opts)
+  end,
 }
