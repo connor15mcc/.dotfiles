7d4cda70207575e84e03062b90f3d6712b8144f0
Connor McCarthy <connor15mcc@gmail.com>
2025-02-06 11:08:40 -0500

    set up conform formatting

--- .config/nvim-clean/lua/plugins/fmt.lua
+++ .config/nvim-clean/lua/plugins/fmt.lua
@@ -1,26 +1,38 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {},
+	{
+		"williamboman/mason.nvim",
+		opts = {},
 	},
 	{
-    "stevearc/conform.nvim",
-    dependencies = { "mason.nvim" },
-    lazy = true,
-    cmd = "ConformInfo",
-    keys = {
-      {
-        "<leader>cF",
-        function()
-          require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-        end,
-        mode = { "n", "v" },
-        desc = "Format Injected Langs",
-      },
+		"stevearc/conform.nvim",
+		dependencies = { "mason.nvim" },
+		lazy = true,
+		cmd = "ConformInfo",
+		keys = {
+			{
+				"<leader>cF",
+				function()
+					require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+				end,
+				mode = { "n", "v" },
+				desc = "Format Injected Langs",
+			},
 		},
-  },
-  {
-    "zapling/mason-conform.nvim",
-    opts = {},
-  },
+		opts = {
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				go = { "goimports", "gofumpt" },
+			},
+		},
+	},
+	{
+		"zapling/mason-conform.nvim",
+		opts = {
+			quiet_mode = false,
+		},
+	},
 }
