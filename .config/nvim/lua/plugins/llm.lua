9270ab83c7a306d9ae81744838fd35204b7db9d7
Connor McCarthy <connor15mcc@gmail.com>
2025-02-25 13:14:27 -0500

    Change keymap so-as to not override default `<C-a>`

--- .config/nvim/lua/plugins/llm.lua
+++ .config/nvim/lua/plugins/llm.lua
@@ -10,9 +10,8 @@ return {
 			log_level = "DEBUG",
 		},
 		keys = {
-			{ "<C-a>",     "<cmd>CodeCompanionActions<cr>",     noremap = true, silent = true },
-			{ "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", noremap = true, silent = true },
-			{ "ga",        "<cmd>CodeCompanionChat Add<cr>",    mode = "v",     noremap = true, silent = true },
+			{ "<leader>a", "<cmd>CodeCompanionActions<cr>",  noremap = true, silent = true },
+			{ "ga",        "<cmd>CodeCompanionChat Add<cr>", mode = "v",     noremap = true, silent = true },
 		},
 	},
 }
