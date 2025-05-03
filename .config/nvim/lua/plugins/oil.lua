263a4d1ba0ba9f9aa7c248560b284a8553d0f87e
Connor McCarthy <connor15mcc@gmail.com>
2025-05-03 14:43:11 -0400

    add `K` mapping to grab file path in oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,22 +1,27 @@
 return {
-  "stevearc/oil.nvim",
-  lazy = false,
-  opts = {
-    columns = {
-      "mtime",
-    },
-    skip_confirm_for_simple_edits = true,
-    prompt_save_on_select_new_entry = false,
-    view_options = {
-      show_hidden = true,
-    },
-  },
-  keys = {
-    {
-      "<leader>e",
-      "<cmd>Oil<cr>",
-      desc = "Open current dir",
-    },
-  },
+	"stevearc/oil.nvim",
+	lazy = false,
+	opts = {
+		columns = {
+			"mtime",
+		},
+		skip_confirm_for_simple_edits = true,
+		prompt_save_on_select_new_entry = false,
+		view_options = {
+			show_hidden = true,
+		},
+		keymaps = {
+			['K'] = {
+				"actions.yank_entry",
+				desc = "Copy path to clipboard",
+			}
+		}
+	},
+	keys = {
+		{
+			"<leader>e",
+			"<cmd>Oil<cr>",
+			desc = "Open current dir",
+		},
+	},
 }
-
