2b81243d9f3e2b8fdeb30d89650aa652323b46db
Connor McCarthy <connor15mcc@gmail.com>
2025-05-06 14:21:45 -0400

    delay setting clipboard for faster startup

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,33 +1,36 @@
-local opt = vim.opt
-
 -- TODO: should come and short / break up by use
-opt.linebreak = true
-opt.scrolloff = 5
-opt.textwidth = 80
-opt.colorcolumn = "80"
-opt.wrap = false
-opt.sidescroll = 0
-opt.directory = "."
-opt.signcolumn = "no"
-opt.conceallevel = 0
-opt.undofile = true
-opt.spell = true
-opt.spellcapcheck = ""
-opt.cmdheight = 0
-opt.grepformat = "%f:%l:%m"
-opt.clipboard = "unnamedplus"
-opt.inccommand = "nosplit"
-opt.list = true -- show invisible chars
-opt.smartcase = true
-opt.virtualedit = "block"
-opt.splitright = true
-opt.splitbelow = true
+vim.opt.linebreak = true
+vim.opt.scrolloff = 5
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "80"
+vim.opt.wrap = false
+vim.opt.sidescroll = 0
+vim.opt.directory = "."
+vim.opt.signcolumn = "no"
+vim.opt.conceallevel = 0
+vim.opt.undofile = true
+vim.opt.spell = true
+vim.opt.spellcapcheck = ""
+vim.opt.cmdheight = 0
+vim.opt.grepformat = "%f:%l:%m"
+vim.opt.inccommand = "nosplit"
+vim.opt.list = true -- show invisible chars
+vim.opt.smartcase = true
+vim.opt.virtualedit = "block"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Make yank clipboard work w/ system clipboard. plus is for linux to ensure it uses the copy clipboard not the selection.
+-- The use of a function pushes this until after UiEnter which lowers startup time according to https://github.com/nvim-lua/kickstart.nvim/blob/d350db2449da40df003c40d440f909d74e2d4e70/init.lua#L115-L116
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
 
 -- handle tabs:
-opt.autoindent = true
-opt.expandtab = false
-opt.tabstop = 2
-opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.filetype.add({
 	pattern = {
