08192a25b6b1cce1bb506b436d8665ce19fe9cec
Connor McCarthy <connor15mcc@gmail.com>
2025-05-06 14:27:26 -0400

    load nvim-lspconfig lazily
    this was one of the slowest plugins to load
    
    also formats

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,29 +1,30 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {},
+	{
+		"williamboman/mason.nvim",
+		opts = {},
 	},
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {},
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- local lspconfig = require("lspconfig")
-      local mason = require("mason")
-      mason.setup()
-      require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup {}
-        end,
-        -- Next, you can provide a dedicated handler for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-        ["rust_analyzer"] = function ()
-					require("lspconfig").rust_analyzer.setup{
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		config = function()
+			-- local lspconfig = require("lspconfig")
+			local mason = require("mason")
+			mason.setup()
+			require("mason-lspconfig").setup_handlers {
+				-- The first entry (without a key) will be the default handler
+				-- and will be called for each installed server that doesn't have
+				-- a dedicated handler.
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup {}
+				end,
+				-- Next, you can provide a dedicated handler for specific servers.
+				-- For example, a handler override for the `rust_analyzer`:
+				["rust_analyzer"] = function()
+					require("lspconfig").rust_analyzer.setup {
 						settings = {
 							["rust-analyzer"] = {
 								rustfmt = {
@@ -32,8 +33,8 @@ return {
 							}
 						}
 					}
-        end
-      }
-    end,
-  },
+				end
+			}
+		end,
+	},
 }
