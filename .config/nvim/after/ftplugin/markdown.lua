ea55d0e4de14f2aa54af937747b75fb0cc1f3bb7
Connor McCarthy <connor15mcc@gmail.com>
2025-09-05 19:52:27 -0400

    remove LSPB client

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,15 +1,2 @@
 vim.cmd('setlocal tabstop=2')
 vim.cmd('setlocal shiftwidth=2')
-
-vim.notify "attaching LSP"
-
-local client = vim.lsp.start_client {
-  name = "lspb",
-  cmd = { "/Users/connormccarthy/projects/lspb/target/debug/lspb" },
-}
-if not client then
-  vim.notify "client is borked"
-end
-vim.lsp.buf_attach_client(0, client)
-
-vim.notify "client should be attached"
