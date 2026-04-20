59778fb9c7c8f3a0031d5cfb8b4601750b601a35
Connor McCarthy <connor15mcc@gmail.com>
2026-04-20 09:47:52 -0400

    darwin compat

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,7 +20,7 @@ bind v,v edit_command_buffer
 bind --mode insert !,! bang_bang
 
 # ssh-agent (I'm sure there's a better way but)
-if not pgrep --full ssh-agent | string collect >/dev/null
+if not pgrep ssh-agent | string collect >/dev/null
   eval (ssh-agent -c)
   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
