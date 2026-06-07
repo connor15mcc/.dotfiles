b880ecd457b2d4060774be52d815b2ae83b4bf5d
Connor McCarthy <connor15mcc@gmail.com>
2026-06-07 18:35:52 -0400

    nvim: cleanup LSP keybinds

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -57,15 +57,10 @@ local diagnostic_goto = function(next, severity)
 		go({ severity = severity })
 	end
 end
-map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map("n", "grd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
 map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
 map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
 map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-
--- LSP:
-map("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
-map("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
-map("n", "grd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
