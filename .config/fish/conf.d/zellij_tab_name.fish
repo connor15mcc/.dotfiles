4ca6d70263544ba892c20120498921bbf1beb023
Connor McCarthy <connor15mcc@gmail.com>
2026-08-11 18:49:34 -0400

    perf(fish): perform zellij tab renames in bg
    disown 'em

--- .config/fish/conf.d/zellij_tab_name.fish
+++ .config/fish/conf.d/zellij_tab_name.fish
@@ -1,7 +1,11 @@
 function zellij_tab_name --on-variable PWD
-  if set -q ZELLIJ
-    command nohup zellij action rename-pane (basename $PWD) >/dev/null 2>&1
-    command nohup zellij action rename-tab (basename $PWD) >/dev/null 2>&1
+  if test "$ZELLIJ" = 1
+    set -l name (basename $PWD)
+    begin
+      command zellij action rename-pane $name
+      command zellij action rename-tab $name
+    end >/dev/null 2>&1 &
+    disown
   end
 end
 
