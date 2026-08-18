1f68f79d7cd13c62754c6e0ddac6dee7822d862f
Connor McCarthy <connor15mcc@gmail.com>
2026-08-18 17:27:15 -0400

    perf(fish): speed up jj prompt by only spawning 1x

--- .config/fish/functions/fish_jj_prompt.fish
+++ .config/fish/functions/fish_jj_prompt.fish
@@ -3,20 +3,18 @@ function fish_jj_prompt
     return 1
   end
 
-  if not jj --ignore-working-copy root --quiet &>/dev/null
-    return 1
-  end
-
   set -l info $(jj --ignore-working-copy log \
     --no-graph \
     --revisions @\
     --template '
-      change_id.shortest() ++ 
+      change_id.shortest() ++
       if(description, " " ++ description.first_line())
     ' 2>/dev/null
   );
 
   if test -n "$info"
     printf ' (jj:%s)' $info
+  else
+    return 1
   end
 end
