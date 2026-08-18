d810f33492a9a985f0153a014b70811bb0324d72
Connor McCarthy <connor15mcc@gmail.com>
2026-08-18 19:19:19 -0400

    perf(fish): caching `jj` prompt
    not super clear this is actually worth it to me, but does seem faster
    😁

--- .config/fish/functions/fish_jj_prompt.fish
+++ .config/fish/functions/fish_jj_prompt.fish
@@ -1,6 +1,30 @@
 function fish_jj_prompt
+  # locate the repo root with builtins only
+  set -l root $PWD
+  while not test -d $root/.jj
+    if test $root = /
+      return 1
+    end
+    set root (path dirname $root)
+  end
+
   if not command -sq jj
-    return 1
+    printf ' (jj:uninstalled)'
+    return 0
+  end
+
+  # our prompt can only change via changes to op-heads (because we ignore
+  # working copy changes via `--no-working-copy), so this is a perfect hash key
+  set -l heads $root/.jj/repo/op_heads/heads/*
+  if test (count $heads) -eq 0
+    printf ' (jj:no op_heads under %s/.jj/repo)' $root
+    return 0
+  end
+  set -l key (string join ' ' $heads)
+
+  if test "$key" = "$__fish_jj_prompt_key"
+    printf ' (jj:%s)' $__fish_jj_prompt_val
+    return 0
   end
 
   set -l info $(jj --ignore-working-copy log \
@@ -12,9 +36,12 @@ function fish_jj_prompt
     ' 2>/dev/null
   );
 
-  if test -n "$info"
-    printf ' (jj:%s)' $info
-  else
-    return 1
+  if test -z "$info"
+    printf ' (jj:log failed in %s)' $root
+    return 0
   end
+
+  set -g __fish_jj_prompt_key $key
+  set -g __fish_jj_prompt_val $info
+  printf ' (jj:%s)' $info
 end
